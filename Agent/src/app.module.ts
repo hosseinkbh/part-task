25e921d45fbe346138b08307885fc9e65ef512b6
hosseinkbh <hosseinkhodabandeh01@gmai.com>
2024-11-22 14:28:39 +0330

    fix app module of agent service and adding base of docker file

--- Agent/src/app.module.ts
+++ Agent/src/app.module.ts
@@ -1,30 +1,36 @@
-import {Module} from '@nestjs/common';
-import {AppService} from './app.service';
-import {ClientsModule, Transport} from '@nestjs/microservices';
-import {ConfigModule, ConfigService} from '@nestjs/config';
-import EnvironmentVariables from './helpers/envCheck';
-import {ScheduleModule} from '@nestjs/schedule';
+import { Module } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import EnvironmentVariables from "./helpers/envCheck";
+import { ScheduleModule } from "@nestjs/schedule";
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({isGlobal: true, validate: EnvironmentVariables.validate}),
-        ScheduleModule.forRoot(),
-        ClientsModule.registerAsync([{
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            name: "KAFKA_CLIENT",
-            useFactory: (configService: ConfigService<EnvironmentVariables, true>) => ({
-                transport: Transport.KAFKA,
-                options: {
-                    client: {
-                        brokers: configService.getOrThrow("KAFKA_BROKER"),
-                        clientId: configService.getOrThrow("KAFKA_CLIENT_ID"),
-                    },
-                },
-            })
-        }]),
-    ],
-    providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: EnvironmentVariables.validate,
+    }),
+    ScheduleModule.forRoot(),
+    ClientsModule.registerAsync([
+      {
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        name: "CLIENT_MODULE",
+        useFactory: (
+          configService: ConfigService<EnvironmentVariables, true>,
+        ) => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              brokers: configService.getOrThrow("KAFKA_BROKER"),
+              clientId: configService.getOrThrow("KAFKA_CLIENT_ID"),
+            },
+          },
+        }),
+      },
+    ]),
+  ],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
--- Agent/src/app.service.ts
+++ Agent/src/app.service.ts
@@ -1,30 +1,34 @@
-import {Inject, Injectable} from '@nestjs/common';
-import {ClientKafka} from '@nestjs/microservices';
-import {ConfigService} from '@nestjs/config';
-import {Interval} from '@nestjs/schedule';
-import {faker} from '@faker-js/faker';
+import { Inject, Injectable } from "@nestjs/common";
+import { ClientKafka } from "@nestjs/microservices";
+import { ConfigService } from "@nestjs/config";
+import { Interval } from "@nestjs/schedule";
+import { faker } from "@faker-js/faker";
 
 @Injectable()
 export class AppService {
+  private KAFKA_TOPIC = this.configService.getOrThrow("KAFKA_TOPIC");
 
-    private KAFKA_TOPIC = this.configService.getOrThrow('KAFKA_TOPIC');
+  constructor(
+    @Inject("CLIENT_MODULE")
+    private readonly ClientModule: ClientKafka,
+    private readonly configService: ConfigService,
+  ) {}
 
-    constructor(
-        @Inject('KAFKA_CLIENT')
-        private readonly kafkaClient: ClientKafka,
-        private readonly configService: ConfigService,
-    ) {
-    }
-
-    @Interval(200)
-    async sendData() {
-        const data = {
-            name: faker.helpers.arrayElement(["temperature", "speed", "volt", "noise", "light"]),
-            value: faker.number.int({min: -1000, max: 1000})
-        }
-        this.kafkaClient.emit(this.KAFKA_TOPIC, {
-            agentId: this.configService.getOrThrow("AGENT_ID"),
-            data: data
-        });
-    }
+  @Interval(200)
+  async sendData() {
+    const data = {
+      name: faker.helpers.arrayElement([
+        "temperature",
+        "speed",
+        "volt",
+        "noise",
+        "light",
+      ]),
+      value: faker.number.int({ min: -1000, max: 1000 }),
+    };
+    this.ClientModule.emit(this.KAFKA_TOPIC, {
+      agentId: this.configService.getOrThrow("AGENT_ID"),
+      data: data,
+    });
+  }
 }
