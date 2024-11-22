9ceea4ead80512cc026ce3aa9d5e605daaa0c2ee
hosseinkbh <hosseinkhodabandeh01@gmai.com>
2024-11-22 14:30:18 +0330

    fix controllers for proccess service

--- Proccess/src/modules/Event/event.controller.ts
+++ Proccess/src/modules/Event/event.controller.ts
@@ -1,17 +1,14 @@
-import {Controller} from "@nestjs/common";
+import { Controller } from "@nestjs/common";
 import EventService from "./event.service";
-import { EventPattern} from "@nestjs/microservices";
-import {ConfigService} from "@nestjs/config";
-import {EventDataDto} from "../../dto/event.dto";
+import { EventPattern } from "@nestjs/microservices";
+import { EventDataDto } from "../../dto/event.dto";
 
 @Controller()
-export default class EventController{
-    constructor(private readonly eventService: EventService,
-        private readonly configService: ConfigService
-    ) {
-    }
-    @EventPattern(process.env.KAFKA_BROKER)
-    async getEvent(data: EventDataDto) {
-        await this.eventService.eventHandler(data)
-    }
+export default class EventController {
+  constructor(private readonly eventService: EventService) {}
+
+  @EventPattern(process.env.KAFKA_BROKER)
+  async getEvent(data: EventDataDto) {
+    await this.eventService.eventHandler(data);
+  }
 }
